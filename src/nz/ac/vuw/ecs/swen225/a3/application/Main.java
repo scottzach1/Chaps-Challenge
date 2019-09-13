0bd0d64e634ad782e240315bd48c618638d8ae88
DurantZac <zdurantnz@gmail.com>
2019-09-14 11:28:16 +1200

    Create board class

--- src/nz/ac/vuw/ecs/swen225/a3/application/Main.java
+++ src/nz/ac/vuw/ecs/swen225/a3/application/Main.java
@@ -1,18 +1,21 @@
 package nz.ac.vuw.ecs.swen225.a3.application;
 
+import nz.ac.vuw.ecs.swen225.a3.maze.Board;
+
 /**
- *   Chip and Chap.
- *
- *   Chap’s challenge is a creative clone of the (first level of the)
- *   1989 Atari game Chips Challenge. To learn more about Chip’s Challenge.
+ * Chip and Chap.
+ * <p>
+ * Chap’s challenge is a creative clone of the (first level of the)
+ * 1989 Atari game Chips Challenge. To learn more about Chip’s Challenge.
  */
 public class Main {
 
-    /**
-     * Main invocation point for running the game.
-     * @param args
-     */
-    public static void main(String[] args) {
-        System.out.println("Todo: Make game.");
-    }
+  /**
+   * Main invocation point for running the game.
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    new Board();
+  }
 }
