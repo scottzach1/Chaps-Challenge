4d2953d7fc530efea88037d32787f66311e0f6bc
Zac Scott <scottzach1@myvuw.ac.nz>
2019-09-13 17:08:52 +1200

    Setup Project.
    + Added assets directory.
    + Added planning directory.
    + Added src/nz/ac/vuw/ecs/swen225/a3/ project code directory.
    + Added renderer, persistence, application, and maze packages to previous project code directory.

---
+++ src/nz/ac/vuw/ecs/swen225/a3/application/Main.java
@@ -0,0 +1,18 @@
+package nz.ac.vuw.ecs.swen225.a3.application;
+
+/**
+ *   Chip and Chap.
+ *
+ *   Chap’s challenge is a creative clone of the (first level of the)
+ *   1989 Atari game Chips Challenge. To learn more about Chip’s Challenge.
+ */
+public class Main {
+
+    /**
+     * Main invocation point for running the game.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Todo: Make game.");
+    }
+}
