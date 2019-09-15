b6c489d3901217a93b268891083467ac10ded077
Jacob Fraser <fraserjaco@ecs.vuw.ac.nz>
2019-09-15 15:29:48 +1200

    Upload New JSON class

---
+++ src/nz/ac/vuw/ecs/swen225/a3/persistence/JsonReadWrite.java
@@ -0,0 +1,18 @@
+package nz.ac.vuw.ecs.swen225.a3.persistence;
+
+import nz.ac.vuw.ecs.swen225.a3.maze.Board;
+
+/**.
+ * @author Group.
+ *
+ */
+public class JsonReadWrite {
+  
+  /**.
+   * @param b = the board.
+   */
+  public JsonReadWrite(Board b) {
+    System.out.println("Todo: Output board information into JSON file.");
+  }
+
+}
